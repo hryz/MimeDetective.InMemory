95581ca88318d3bb5d1beb5b5f1cb82db4b06efa
Lukas Eßmann <l.essmann@binarypanther.com>
2019-09-11 14:50:18 +0200

    Added support for WOFF files

--- src/MimeDetective.InMemory/MimeTypes.cs
+++ src/MimeDetective.InMemory/MimeTypes.cs
@@ -20,7 +20,7 @@ namespace MimeDetective.InMemory
                 PYC_3_4_A1_1, PYC_3_4_A1_2, PYC_3_4_A1_3, PYC_3_4_A1_4, PYC_3_4_A4_1, PYC_3_4_A4_2, PYC_3_4_RC2, PYC_3_5_A0, PYC_3_5_B1,
                 PYC_3_5_B2_1, PYC_3_5_B2_2, PYC_3_5_2, PYC_3_6_A0_1, PYC_3_6_A0_2, PYC_3_6_A1_1, PYC_3_6_A1_2, PYC_3_6_A1_3, PYC_3_6_A1_4,
                 PYC_3_6_B1_1, PYC_3_6_B1_2, PYC_3_6_B1_3, PYC_3_6_B2, PYC_3_6_RC2, PYC_3_7_A1, PYC_3_7_A2, PYC_3_7_A4, PYC_3_7_B1, PYC_3_7_B5,
-                MKVAS3D_WEBM, ISO_1, ISO_2, ISO_3, MPEGTS, VMDK, NVRAM, PCAP_1, PCAP_2, PCAPNG, SQLITEDB, TTF
+                MKVAS3D_WEBM, ISO_1, ISO_2, ISO_3, MPEGTS, VMDK, NVRAM, PCAP_1, PCAP_2, PCAPNG, SQLITEDB, TTF, WOFF
             };
         }
 
@@ -192,6 +192,9 @@ namespace MimeDetective.InMemory
 
         //TTF Font
         public static readonly FileType TTF = new FileType(new byte?[] { 0x00, 0x01, 0x00, 0x00, 0x00 }, "ttf,ttc", "application/x-font-ttf");
+
+        //WOFF font
+        public static readonly FileType WOFF = new FileType(new byte?[] { 0x77, 0x4f, 0x46, 0x46 }, "woff", "font/woff");
 
         #endregion
         #region Python
